482d4e5692191708eb0085620a04a81052eb9ac0
unknown <helen.vielma@gmail.com>
2020-02-10 19:22:58 -0300

    Se agrega la funcionalidad de asignar imagenes random a los divs

---
+++ src/js/validaciones.js
@@ -0,0 +1,24 @@
+//desordena el arreglo de las fotos,como son pares de fotos, se repiten 2 veces en el arreglo
+
+function llenarArreglo() {
+    var fotos = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    //esto desordena el arreglo
+    fotos = fotos.sort(function() {
+        return Math.random() - 0.5
+    });
+
+
+    return fotos;
+
+}
+
+//esto carga la imagen en el div en cuanto al arreglo aleatorio
+function cargarImagen() {
+    var pokemon = llenarArreglo();
+    var i = 0;
+    while (i < pokemon.length) {
+        var aleatorio = pokemon[i];
+        document.getElementById(i).innerHTML = "<img src=src/img/" + aleatorio + ".png>";
+        i++;
+    }
+}
